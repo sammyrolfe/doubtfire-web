28e83bd745973c101349e15017ace9d5aa9a1986
Sam <sam-rolfe@hotmail.com>
2024-08-10 21:14:03 +1000

    feat:create-containers

--- src/app/courseflow/coursemap/coursemap.component.ts
+++ src/app/courseflow/coursemap/coursemap.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { DoubtfireConstants } from 'src/app/config/constants/doubtfire-constants';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'f-welcome',
+  selector: 'coursemap',
   templateUrl: './coursemap.component.html',
   styleUrls: ['./coursemap.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MatListModule, DragDropModule],
 })
 export class CoursemapComponent implements OnInit {
-  constructor(private constants: DoubtfireConstants) {}
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  trimester1 = ['Exercise', 'Study'];
+  trimester2 = ['Read a book'];
+
+  constructor() {}
+
   ngOnInit(): void {}
 
-  public externalName = this.constants.ExternalName;
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
